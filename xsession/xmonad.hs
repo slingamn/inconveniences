56cd6f3b5aa039e77f0bd9df2b9f423f35621537
Shivaram Lingamneni <slingamn@cs.stanford.edu>
2016-03-19 21:48:39 -0700

    remove card number from amixer invocations

--- xsession/xmonad.hs
+++ xsession/xmonad.hs
@@ -14,9 +14,9 @@ import System.Taffybar.Hooks.PagerHints (pagerHints)
 -- "just treat Haskell as a domain-specific language for window management"
 
 lowerVolume    = "<XF86AudioLowerVolume>"
-lowerVolumeCMD = "amixer -c 0 set Master 1-"
+lowerVolumeCMD = "amixer set Master 1%-"
 raiseVolume    = "<XF86AudioRaiseVolume>"
-raiseVolumeCMD = "amixer -c 0 set Master 1+ unmute"
+raiseVolumeCMD = "amixer set Master 1%+ unmute"
 muteVolume     = "<XF86AudioMute>"
 muteVolumeCMD  = "amixer set Master toggle"
 
